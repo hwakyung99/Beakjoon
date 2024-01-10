32ca0d292fd2fdbc8c30717bae4f0645c290bf16
hwakyung99 <58030720+hwakyung99@users.noreply.github.com>
2024-01-10 12:44:20 +0900

    [Silver V] Title: 수들의 합 5, Time: 188 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/2018. 수들의 합 5/수들의 합 5.py
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.py
@@ -2,21 +2,18 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-s = [0] * (N + 1)
-ans = 0
+l, r, s, ans = 1, 1, 1, 0
 
-for i in range(1, N + 1):
-    s[i] = s[i - 1] + i
-
-p1, p2 = 0, 1
-while p1 < N + 1 and p2 < N + 1:
-    tmp = s[p2] - s[p1]
-    if tmp < N:
-        p2 += 1
-    elif tmp > N:
-        p1 += 1
+while l < N + 1 and r < N + 1:
+    if s < N:
+        r += 1
+        s += r
+    elif s > N:
+        s -= l
+        l += 1
     else:
         ans += 1
-        p1 += 1
+        s -= l
+        l += 1
 
 print(ans)
