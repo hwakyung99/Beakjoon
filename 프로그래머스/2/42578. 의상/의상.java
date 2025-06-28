4ddd043c0590e931c6999bfb45149b05751c3312
LeeHwakyung <58030720+hwakyung99@users.noreply.github.com>
2025-06-29 00:48:52 +0900

    [level 2] Title: 의상, Time: 0.10 ms, Memory: 85.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.util.Map.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        Map<String, Integer> map = new HashMap<>();
+        
+        for(String[] cloth : clothes) {
+            map.put(cloth[1], map.getOrDefault(cloth[1], 1) + 1);
+        }
+        
+        for(int value : map.values()) {
+            answer *= value;
+        }
+        
+        return answer - 1;
+    }
+}
